f5f4bda6a7a2832d88e14f6ef849d7579accae41
krishmungase <mungasekrishna8@gmail.com>
2025-01-13 22:29:55 +0530

    add desktop view

--- src/common/desktop-view.jsx
+++ src/common/desktop-view.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { TbLayoutSidebarLeftCollapseFilled } from "react-icons/tb";
+import SideBarLinks from '../config/side-bar-links';
+import { NavLink } from 'react-router-dom';
 
 const DesktopDashboardSidebar = ({ openSideBar, setOpenSideBar }) => {
 
@@ -10,11 +12,46 @@ const DesktopDashboardSidebar = ({ openSideBar, setOpenSideBar }) => {
       </div>
       {
         openSideBar ?
-          <h1>Hello</h1>
-          :
-
           (
-            <h1>Hi</h1>
+            <div className="hidden lg:block">
+              <div className="px-4 w-full h-[calc(100vh_-140px)] mt-8 flex justify-between flex-col">
+                <div className="flex space-y-1 items-center flex-col justify-center">
+                  {
+                    SideBarLinks.map((link) => (
+                      <NavLink
+                        key={link.id}
+                        // to={link.path}
+                        className={`${location.pathname === link.path ? "bg-rich-700" : ""} flex items-center px-4 py-3 hover:bg-rich-400 hover:text-white transition-all rounded-[13px] w-full`}
+                      >
+                        <link.icon className="size-6 text-rich-900" />
+                        <span className="pl-4 text-lg text-rich-900 font-light tracking-wider">
+                          {link.title}
+                        </span>
+                      </NavLink>
+                    ))
+                  }
+                </div>
+              </div>
+            </div>
+          )
+          :
+          (
+            <div className="">
+              <div className="flex items-center h-[calc(100vh_-140px)] justify-between flex-col mt-8 space-y-6">
+                <div className="flex items-center flex-col justify-center space-y-4">
+                  {
+                    SideBarLinks.map((link) => (
+                      <NavLink
+                        key={link.id}
+                        className={`${location.pathname === link.path ? "bg-rich-700" : ""} flex items-center px-4 py-3 hover:bg-rich-400 hover:text-white transition-all rounded-[13px] w-full`}
+                      >
+                        <link.icon className="size-6 text-richblack-200" />
+                      </NavLink>
+                    ))
+                  }
+                </div>
+              </div>
+            </div>
           )
       }
     </div>
