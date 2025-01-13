9d5bda2af3b05fa70330baa8a6ddc4d82820c738
krishmungase <mungasekrishna8@gmail.com>
2025-01-13 23:13:21 +0530

    add homepage content

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -4,10 +4,17 @@ import Navbar from './navbar'
 
 const HomePage = () => {
   return (
-    <div className='text-black'>
+    <div className=''>
       <Navbar />
+      <div className="mt-16 px-4 flex flex-col items-center justify-center">
+        <h1 className="text-5xl mt-16 font-semibold text-justify">
+          Innovating the Future,
+          <br className="" />
+          &nbsp;  &nbsp;One Step at a Time
+        </h1>
+      </div>
     </div>
   )
 }
 
-export default HomePage
+export default HomePage 
