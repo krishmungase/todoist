aa32b876db9f0331a0703383a40683c00a39fd52
krishmungase <mungasekrishna8@gmail.com>
2025-01-13 23:31:32 +0530

    add framer

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,17 +1,25 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Navbar from './navbar'
+import { motion } from 'framer-motion';
+
+
 
 const HomePage = () => {
   return (
     <div className=''>
       <Navbar />
       <div className="mt-16 px-4 flex flex-col items-center justify-center">
-        <h1 className="text-5xl mt-16 font-semibold text-justify">
+        <motion.h1
+          className="text-xl md:text-3xl lg:text-5xl mt-16 font-semibold text-justify leading-relaxed"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 2 }}
+          transition={{ duration: 5 }}
+        >
           Innovating the Future,
-          <br className="" />
-          &nbsp;  &nbsp;One Step at a Time
-        </h1>
+          <br />
+          &nbsp;&nbsp;One Step at a Time
+        </motion.h1>
       </div>
     </div>
   )
