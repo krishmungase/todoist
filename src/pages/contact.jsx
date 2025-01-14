f0b97760706ea974abf2c8a291968099b1ae2b21
krishmungase <mungasekrishna8@gmail.com>
2025-01-14 08:56:17 +0530

    add pages

---
+++ src/pages/contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div className="mt-16 px-4 flex flex-col items-center justify-center">ContactPage</div>
+  )
+}
+
+export default ContactPage
---
+++ src/pages/learn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LearnPage = () => {
+  return (
+    <div className="mt-16 px-4 flex flex-col items-center justify-center">LearnPage</div>
+  )
+}
+
+export default LearnPage
