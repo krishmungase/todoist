94ef6f9a0ea71ac51a374a009a020b2637d9694a
krishmungase <mungasekrishna8@gmail.com>
2025-01-15 00:35:08 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,11 @@ import DashboardLayout from "./layout/dashboard-layout"
 import AboutPage from "./pages/about"
 import ContactPage from "./pages/contact"
 import LearnPage from "./pages/learn"
+import TaskPage from "./components/tasks/task-page"
 
 const App = () => {
+
+
   return (
     <div className="w-screen min-h-screen flex flex-col">
       <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div></div>
@@ -19,7 +22,7 @@ const App = () => {
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/learn" element={<LearnPage />} />
-          <Route path="/tasks" element={<LearnPage />} />
+          <Route path="/tasks" element={<TaskPage />} />
           <Route path="/recent" element={<LearnPage />} />
           <Route path="/profile" element={<LearnPage />} />
           <Route path="/setting" element={<LearnPage />} />
