275273c311a311407975859c5c68ad8fa7d2fcc3
krishmungase <mungasekrishna8@gmail.com>
2025-01-14 08:55:41 +0530

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import { Route, Routes } from "react-router-dom"
 import Navbar from "./pages/navbar"
 import HomePage from "./pages/home-page"
 import DashboardLayout from "./layout/dashboard-layout"
+import AboutPage from "./pages/about"
+import ContactPage from "./pages/contact"
+import LearnPage from "./pages/learn"
 
 const App = () => {
   return (
     <div className="w-screen min-h-screen flex flex-col">
-      <div className="absolute inset-0 -z-10 size-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-        <div className="absolute inset-x-0 top-0 -z-10 m-auto size-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]" />
-      </div>
+      <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div></div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<DashboardLayout />}>
           <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/learn" element={<LearnPage />} />
         </Route>
       </Routes>
     </div >
