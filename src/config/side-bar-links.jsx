bcba0ba849910c88d4a6a3788e145f9d74af56bc
krishmungase <mungasekrishna8@gmail.com>
2025-01-14 22:41:29 +0530

    add path in sidebar links

--- src/config/side-bar-links.jsx
+++ src/config/side-bar-links.jsx
@@ -7,21 +7,25 @@ const SideBarLinks = [
   {
     id: 1,
     title: "Profile",
+    path: "/profile",
     icon: CgProfile,
   },
   {
     id: 2,
     title: "Tasks",
+    path: "/tasks",
     icon: LuListTodo
   },
   {
     id: 3,
     title: "Recent Activities",
+    path: "/recent",
     icon: HiTable,
   },
   {
     id: 4,
     title: "Setting",
+    path: "/setting",
     icon: Settings
   },
 ];
