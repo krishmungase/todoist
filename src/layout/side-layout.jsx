afb3048ca6b85f367ace878a1759bf6cd64683e5
krishmungase <mungasekrishna8@gmail.com>
2025-01-14 08:56:06 +0530

    add framer

--- src/layout/side-layout.jsx
+++ src/layout/side-layout.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import DesktopDashboardSidebar from '../common/desktop-view';
 import MobilDashboardSidebar from '../common/mobile-view';
-// import MobilDashboardSidebar from './mobile-dashboard'
+import { motion } from 'framer-motion';
 
 const DashboardSidebar = () => {
   const [openSideBar, setOpenSideBar] = useState(true);
 
   return (
-    <div className={`w-[100px] ${openSideBar ? "lg:w-[350px]" : "lg:w-[100px]"} p-3`}>
+    <motion.div
+      className={`p-3`}
+      initial={{ width: '100px' }}
+      animate={{ width: openSideBar ? '350px' : '100px' }}
+      transition={{ duration: 0.5 }}
+    >
       <div className="bg-rich-500 h-[calc(100vh_-28px)] rounded-lg py-8 w-full">
         <MobilDashboardSidebar />
         <DesktopDashboardSidebar openSideBar={openSideBar} setOpenSideBar={setOpenSideBar} />
       </div>
-    </div>
+    </motion.div>
   );
 }
 
