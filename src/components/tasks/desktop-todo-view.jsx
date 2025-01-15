efc0755752838aa8fb9ea3b4b21cbf4fd36c9954
krishmungase <mungasekrishna8@gmail.com>
2025-01-15 10:04:04 +0530

    color changes

--- src/components/tasks/desktop-todo-view.jsx
+++ src/components/tasks/desktop-todo-view.jsx
@@ -6,8 +6,8 @@ const DesktopTodoList = ({
   refetchTodoList,
 }) => {
   return (
-    <div className="hidden xl:block relative h-[calc(100vh_-140px)] bg-white rounded-lg shadow-sm">
-      <div className="grid grid-cols-3 gap-4 font-medium text-primary bg-gray-100 rounded-tr-lg rounded-tl-lg overflow-hidden px-6 py-2">
+    <div className="hidden xl:block relative h-[calc(100vh_-140px)] bg-turnary rounded-lg shadow-sm">
+      <div className="grid grid-cols-3 gap-4 font-medium text-primary bg-rich-200 rounded-tr-lg rounded-tl-lg overflow-hidden px-6 py-2">
         <h1 className="p-1 w-full text-center">Todo</h1>
         <h1 className="p-1 w-full text-center">Doing</h1>
         <h1 className="p-1 w-full text-center">Completed</h1>
